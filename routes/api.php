166a07281d9af4f5d7ff84295108183675ce156b
Dava DN <ayujas321@gmail.com>
2025-02-10 16:12:57 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //posts
 Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', function (Request $request) {
+        return response()->json($request->user());
+    });
+});
